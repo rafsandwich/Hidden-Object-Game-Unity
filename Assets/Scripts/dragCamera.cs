e22bf72a49e277f825e90cd69b341fbec8648c9b
AZEUS\rafha <rafhall@hotmail.com>
2024-03-01 13:18:54 +0000

    setting player camera bounds

--- Assets/Scripts/dragCamera.cs
+++ Assets/Scripts/dragCamera.cs
@@ -12,6 +12,9 @@ public class dragCamera : MonoBehaviour
 
     private bool _isDragging;
 
+    private Bounds _cameraBounds;
+    private Vector3 _targetPosition;
+
 
     private void Awake()
     {
@@ -31,6 +34,26 @@ public class dragCamera : MonoBehaviour
 
     }
 
+    private void Start()
+    {
+        //get height & width of camera
+        var height = _mainCamera.orthographicSize;
+        var width = height * _mainCamera.aspect;
+
+        var minX = Globals.WorldBounds.min.x + width;
+        var maxX = Globals.WorldBounds.extents.x - width;
+
+        var minY = Globals.WorldBounds.min.y + height;
+        var maxY = Globals.WorldBounds.extents.y - height;
+
+        //set camera bounds
+        _cameraBounds = new Bounds();
+        _cameraBounds.SetMinMax(
+            new Vector3(minX, minY, 0),
+            new Vector3(maxX, maxY, 0)
+        );
+    }
+
     //take care of camera movement
     private void LateUpdate()
     {
@@ -39,8 +62,20 @@ public class dragCamera : MonoBehaviour
 
         _difference = GetMousePosition - transform.position;
 
+        _targetPosition = _origin - _difference;
+        _targetPosition = GetCameraBounds();
+
         //set position of camera
-        transform.position = _origin - _difference;
+        transform.position = _targetPosition;
+    }
+
+    private Vector3 GetCameraBounds()
+    {
+        return new Vector3(
+            Mathf.Clamp(_targetPosition.x, _cameraBounds.min.x, _cameraBounds.max.x),
+            Mathf.Clamp(_targetPosition.y, _cameraBounds.min.y, _cameraBounds.max.y),
+            transform.position.z
+        );
     }
 
     private Vector3 GetMousePosition => _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
