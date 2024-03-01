0d71cc0e230894784386054aa78d33eac9361e26
AZEUS\rafha <rafhall@hotmail.com>
2024-03-01 22:35:38 +0000

    working drag, zoom w/ mouse wheel / buttons + camera constraints

--- Assets/Scripts/CameraDragC.cs
+++ Assets/Scripts/CameraDragC.cs
@@ -11,11 +11,35 @@ public class CameraDragC : MonoBehaviour
     [SerializeField]
     private float zoomStep, minCamSize, maxCamSize;
 
+    [SerializeField]
+    private SpriteRenderer mapRenderer;
+
+    private float mapMinX, mapMaxX, mapMinY, mapMaxY;
+
+
+
     private Vector3 dragOrigin;
+
+    private void Awake()
+    {
+        mapMinX = mapRenderer.transform.position.x - mapRenderer.bounds.size.x / 2f;
+        mapMaxX = mapRenderer.transform.position.x + mapRenderer.bounds.size.x / 2f ;
+
+        mapMinY = mapRenderer.transform.position.y - mapRenderer.bounds.size.y / 2f;
+        mapMaxY = mapRenderer.transform.position.y + mapRenderer.bounds.size.y / 2f;
+    }
 
     private void Update()
     {
         PanCamera();
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            ZoomIn();
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            ZoomOut();
+        }
     }
 
     private void PanCamera()
@@ -33,10 +57,11 @@ public class CameraDragC : MonoBehaviour
             //check mouse position and compare to origin
             Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
 
-            print("origin" + dragOrigin + " newPosition " + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference " + difference);
+            //DEBUG -> print("origin" + dragOrigin + " newPosition " + cam.ScreenToWorldPoint(Input.mousePosition) + " =difference " + difference);
 
             //move camera by that distance
-            cam.transform.position += difference;
+
+            cam.transform.position = ClampCamera(cam.transform.position + difference);
         }  
     }
 
@@ -46,6 +71,9 @@ public class CameraDragC : MonoBehaviour
         float newSize = cam.orthographicSize - zoomStep;
 
         cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+
+        cam.transform.position = ClampCamera(cam.transform.position);
+
     }
 
     public void ZoomOut()
@@ -53,7 +81,25 @@ public class CameraDragC : MonoBehaviour
         float newSize = cam.orthographicSize + zoomStep;
 
         cam.orthographicSize = Mathf.Clamp(newSize, minCamSize, maxCamSize);
+
+        cam.transform.position = ClampCamera(cam.transform.position);
     }
 
+    //size of camera changes every time zoom, calculate final positions where camera can move from scratch each time
+    //should be called every time we want to move camera / zoom has occurred 
+    private Vector3 ClampCamera(Vector3 targetPosition)
+    {
+        float camHeight = cam.orthographicSize;
+        float camWidth = cam.orthographicSize * cam.aspect;
 
+        float minX = mapMinX + camWidth;
+        float maxX = mapMaxX - camWidth;
+        float minY = mapMinY + camHeight;
+        float maxY = mapMaxY - camHeight;
+
+        float newX = Mathf.Clamp(targetPosition.x, minX, maxX);
+        float newY = Mathf.Clamp(targetPosition.y, minY, maxY);
+
+        return new Vector3(newX, newY, targetPosition.z);
+    }
 }
